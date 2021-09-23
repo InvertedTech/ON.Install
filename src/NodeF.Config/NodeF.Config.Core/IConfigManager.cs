2535d41f250729449e60a4565363354649d7265e
Phillip Fisher <phillipfisher@github.com>
2021-09-23 00:24:23 -0500

    Add NodeF.Config

---
+++ src/NodeF.Config/NodeF.Config.Core/IConfigManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeF.Config
+{
+    public interface IConfigManager
+    {
+        void AddTopic();
+        Task LoadAll();
+        Task LoadOne();
+        Task SaveAll();
+        Task SaveOne();
+    }
+}
---
+++ src/NodeF.Config/NodeF.Config.Core/IConfigTopic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeF.Config
+{
+    public interface IConfigTopic
+    {
+        IConfigTopic this[int index] { get; set; }
+    }
+}
