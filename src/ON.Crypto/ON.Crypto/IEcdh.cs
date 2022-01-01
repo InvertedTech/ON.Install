9410fa452d30baccc06095a21ed5df3608dbcf72
Phillip Fisher <phillip_fisher@hotmail.com>
2022-01-01 11:46:44 -0600

    Fix obsolete crypto and add linux crypto

---
+++ src/ON.Crypto/ON.Crypto/IEcdh.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ON.Crypto
+{
+    public interface IEcdh
+    {
+        public byte[] DeriveKeyClient(ECDsa privKey, JsonWebKey serverPubKey);
+
+        public byte[] DeriveKeyServer(JsonWebKey clientPubKey, out string serverPubKey);
+    }
+}
