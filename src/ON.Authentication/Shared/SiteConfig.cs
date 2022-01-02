78e3d6698cb53b609b5a63f97c844c0b86af8532
Phillip Fisher <phillip_fisher@hotmail.com>
2022-01-01 18:44:33 -0600

    Fix layout issues

---
+++ src/ON.Authentication/Shared/SiteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ON
+{
+    public static class SiteConfig
+    {
+        public static string WebsiteName = "Get ON";
+
+        static SiteConfig()
+        {
+            var str = Environment.GetEnvironmentVariable("WEBSITE_NAME", EnvironmentVariableTarget.Process);
+            if (str != null)
+                WebsiteName = str;
+        }
+    }
+}
