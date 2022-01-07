af24e04fa51b5b5f959d903875eab419362b21af
Andrew Mingst <amingst97@gmail.com>
2022-01-07 18:13:47 -0500

    Added react client

---
+++ src/ON.Authorization/StripePayments/proto/randomPackage/UserStreamResponse.ts
@@ -0,0 +1,11 @@
+// Original file: proto/random.proto
+
+import type { User as _randomPackage_User, User__Output as _randomPackage_User__Output } from '../randomPackage/User';
+
+export interface UserStreamResponse {
+  'user'?: (_randomPackage_User)[];
+}
+
+export interface UserStreamResponse__Output {
+  'user'?: (_randomPackage_User__Output)[];
+}
