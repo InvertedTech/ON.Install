31fcef595cf5f5f02302fd2ebaca31e4be57e8ba
Andrew Mingst <amingst97@gmail.com>
2022-01-06 22:38:06 -0500

    created basic server and client

--- src/ON.Authorization/StripePayments/client.js
+++ src/ON.Authorization/StripePayments/client.js
@@ -15,27 +15,15 @@ var protoLoader = require('@grpc/proto-loader')
 const SUBSCRIPTION_PATH = './proto/Authorization/Subscription.proto'
 var subscriptionDefinition = protoLoader.loadSync(SUBSCRIPTION_PATH, options)
 var subscriptionProto = grpc.loadPackageDefinition(subscriptionDefinition)
-var subscriptionService =
+var SubscriptionService =
 	subscriptionProto.ON.Fragments.Authorization.SubscriptionService
 
-// Load Data
-const subscriptions = require('./subscriptions')
-
-const server = new grpc.Server()
-
-server.addService(subscriptionService.service, {
-	getAll: (_, callback) => {
-		callback(null, { subscriptions })
-	},
-})
-
-server.bindAsync(
-	// TODO: Reference port in env
+const client = new SubscriptionService(
 	'127.0.0.1:50051',
-	grpc.ServerCredentials.createInsecure(),
-	(error, port) => {
-		if (error) console.error(error)
-		console.log(`Server running at http://127.0.0.1:${port}`)
-		server.start()
-	}
+	grpc.credentials.createInsecure()
 )
+
+// TODO: Create calls
+console.log(client.getAll)
+
+// module.exports = client;
