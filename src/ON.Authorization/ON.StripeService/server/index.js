c06dcfb720d3f7eb6881a2fad65eea4007b4d52c
Andrew Mingst <amingst97@gmail.com>
2022-01-26 19:04:17 -0500

    added react client

--- src/ON.Authorization/ON.StripeService/server/index.js
+++ src/ON.Authorization/ON.StripeService/server/index.js
@@ -1,27 +1,61 @@
 var grpc = require('grpc')
 const { v4: uuidv4 } = require('uuid')
+var fs = require('fs')
 
 var service = require('../server/proto/stripe_grpc_pb')
 var StripeService = require('../server/proto/stripe_pb')
 const writeBuffer = require('./utils/writeBuffer')
 
-function writeData(call, callback) {
-	let req = call.request.serializeBinary()
+// function writeData(call, callback) {
+// 	let req = call.request.serializeBinary()
 
-	var response = new StripeService.StripeResponse()
-	response.setResponsemessage('server response')
+// 	var response = new StripeService.StripeResponse()
+// 	response.setResponsemessage('server response')
 
-	writeBuffer(req, 'stored.pb')
+// 	writeBuffer(req, 'stored.pb')
 
-	callback(null, response)
+// 	callback(null, response)
+// }
+
+function createPlanRecord() {
+	let plans = new StripeService.PlanList()
+
+	fs.readFile('../plans.json', 'utf8', (error, data) => {
+		if (!error) {
+			plans = JSON.parse(data)
+			for (element in plans) {
+				let item = plans[element]
+				let newPlan = new StripeService.PlanRecord()
+				newPlan.setValue(item.value)
+				newPlan.setName(item.name)
+				newPlan.setPlanid('id')
+				console.log(newPlan)
+				plans.addRecords(newPlan)
+			}
+		} else {
+			console.error(error)
+		}
+	})
+
+	console.log(plans)
+}
+
+function loadPlanRecord(path) {
+	let exists = fs.existsSync(path)
+
+	if (!exists) {
+		console.log('Plans not found, creating file...')
+	} else {
+		console.log('Plans found')
+	}
 }
 
 function main() {
 	var server = new grpc.Server()
 
-	server.addService(service.StripeServiceService, {
-		writeStripeData: writeData,
-	})
+	// server.addService(service.StripeServiceService, {
+	// 	writeStripeData: writeData,
+	// })
 	server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure())
 	server.start()
 
