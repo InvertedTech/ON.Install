6aacb30b3fb9ade298f2f9bc9d4eb67a0d68ec29
Andrew Mingst <amingst97@gmail.com>
2022-04-10 23:35:47 -0400

    Added Video Upload message to protos and Content service

--- src/ON.Content/SimpleCMS/Service/Services/ContentService.cs
+++ src/ON.Content/SimpleCMS/Service/Services/ContentService.cs
@@ -198,5 +198,11 @@ namespace ON.Content.SimpleCMS.Service
                 Content = record.Public
             };
         }
+
+        public Task<UploadVideoRumbleResponse> RumbleUpload(UploadVideoRumbleRequest request, ServerCallContext context)
+        {
+            var user = ONUserHelper.ParseUser(context.GetHttpContext());
+            return null;
+        }
     }
 }
