6b78a67adfcefca07cc50464668ba915ef49e140
Andrew Mingst <amingst97@gmail.com>
2022-04-20 19:16:05 -0400

    Added AppSettings.cs and edited dockerfile

--- src/ON.Content/Video/Service/Program.cs
+++ src/ON.Content/Video/Service/Program.cs
@@ -1,17 +1,42 @@
-using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace ON.Content.Video.Service
+{
+    public class Program
+    {
+        public const string SERVICE_NAME = "VIDEOSERVICE";
+        public const string API_PORT_NAME = "SERVICE__" + SERVICE_NAME + "__API__PORT";
+        public const string GRPC_PORT_NAME = "SERVICE__" + SERVICE_NAME + "__GRPC__PORT";
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-// Add services to the container.
-builder.Services.AddGrpc();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-app.Run();
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.ListenAnyIP(GetPort(API_PORT_NAME), o => o.Protocols = HttpProtocols.Http1);
+                        options.ListenAnyIP(GetPort(GRPC_PORT_NAME), o => o.Protocols = HttpProtocols.Http2);
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+        private static int GetPort(string keyName)
+        {
+            var str = Environment.GetEnvironmentVariable(keyName, EnvironmentVariableTarget.Process);
+            return int.Parse(str);
+        }
+    }
+}
---
+++ src/ON.Content/Video/Service/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using ON.Authentication;
+using ON.Content.Video.Service.Models;
+
+namespace ON.Content.Video.Service
+{
+    public class Startup
+    {
+        private static byte[] PONG_RESPONSE = { (byte)'p', (byte)'o', (byte)'n', (byte)'g' };
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpcHttpApi();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("video", new OpenApiInfo { Title = "Video API" });
+            });
+            services.AddGrpcSwagger();
+
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            services.AddSingleton<IContentDataProvider, FileSystemContentDataProvider>();
+
+            services.AddJwtAuthentication();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.Map("/ping", (app1) => app1.Run(async context => {
+                await context.Response.BodyWriter.WriteAsync(PONG_RESPONSE);
+            }));
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "api/{documentName}/swagger.json";
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/api/video/swagger.json", "Video API");
+                c.RoutePrefix = "api/video";
+            });
+
+            app.UseRouting();
+
+            app.UseJwtAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<BackupService>();
+                endpoints.MapGrpcService<ContentService>();
+                endpoints.MapGrpcService<ServiceOpsService>();
+            });
+        }
+    }
+}
