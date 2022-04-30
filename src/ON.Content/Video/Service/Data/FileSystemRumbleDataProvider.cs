0a6ae716ee01dfcbeecbd82bd1931cbd3a4a8e44
Andrew Mingst <amingst97@gmail.com>
2022-04-30 16:36:32 -0400

    Added filesystemprovider for Rumble, edited protos

---
+++ src/ON.Content/Video/Service/Data/FileSystemRumbleDataProvider.cs
@@ -0,0 +1,37 @@
+﻿using Google.Protobuf;
+using Microsoft.Extensions.Options;
+using ON.Content.Video.Service.Models;
+using ON.Fragments.Content;
+
+namespace ON.Content.Video.Service.Data
+{
+    public class FileSystemRumbleDataProvider : IFileSystemRumbleProvider
+    {
+        private readonly DirectoryInfo dataDir;
+        private readonly FileInfo listFile;
+
+        public FileSystemRumbleDataProvider(IOptions<AppSettings> settings)
+        {
+            var root = new DirectoryInfo(settings.Value.DataStore);
+            root.Create();
+            dataDir = root.CreateSubdirectory("rumble");
+            listFile = new FileInfo(dataDir.FullName + "/rumble-data");
+        }
+
+        public async Task<RumbleData> GetData()
+        {
+            if (!listFile.Exists)
+            {
+                return new RumbleData();
+            }
+
+            var bytes = await File.ReadAllBytesAsync(listFile.FullName);
+            return RumbleData.Parser.ParseFrom(bytes);
+        }
+
+        public async Task SaveData(RumbleData data)
+        {
+            await File.WriteAllBytesAsync(listFile.FullName, data.ToByteArray());
+        }
+    }
+}
---
+++ src/ON.Content/Video/Service/Models/IFileSystemRumbleProvider.cs
@@ -0,0 +1,10 @@
+﻿using ON.Fragments.Content;
+
+namespace ON.Content.Video.Service.Models
+{
+    public interface IFileSystemRumbleProvider
+    {
+        Task<RumbleData> GetData();
+        Task SaveData(RumbleData data);
+    }
+}
