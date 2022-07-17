9c21085e9ef67576e34a0d286602c9cab1dd9d43
Andrew Mingst <amingst97@gmail.com>
2022-07-16 20:14:10 -0400

    Created models for ban and mute

---
+++ src/ON.CreatorDashboard/Service/Models/Ban.cs
@@ -0,0 +1,16 @@
+﻿namespace ON.CreatorDashboard.Service.Models
+{
+    public class Ban
+    {
+        private string id { get; set; }
+        private bool isValid { get; set; } = false;
+        private string userId { get; set; }
+        private string message { get; set; }
+        private string reason { get; set; }
+        private string bannedBy { get; set; }
+        private string unbannedBy { get; set; }
+        private Google.Protobuf.WellKnownTypes.Timestamp dateBanned { get; set; } = new Google.Protobuf.WellKnownTypes.Timestamp();
+        private Google.Protobuf.WellKnownTypes.Duration durationBanned { get; set; }
+        private Google.Protobuf.WellKnownTypes.Timestamp dateUnbanned { get; set; }
+    }
+}
---
+++ src/ON.CreatorDashboard/Service/Models/Mute.cs
@@ -0,0 +1,16 @@
+﻿namespace ON.CreatorDashboard.Service.Models
+{
+    public class Mute 
+    {
+        private string id { get; set; }
+        private bool isValid { get; set; } = false;
+        private string userId { get; set; }
+        private string message { get; set; }
+        private string reason { get; set; }
+        private string mutedBy { get; set; }
+        private string unmutedBy { get; set; }
+        private Google.Protobuf.WellKnownTypes.Timestamp dateMuted { get; set; } = new Google.Protobuf.WellKnownTypes.Timestamp();
+        private Google.Protobuf.WellKnownTypes.Duration durationMuted { get; set; }
+        private Google.Protobuf.WellKnownTypes.Timestamp DateUnmuted { get; set; }
+    }
+}
