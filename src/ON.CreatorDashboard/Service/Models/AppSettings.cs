7691dac0fc467fc44cf3a6078101345e9562935b
Andrew Mingst <amingst97@gmail.com>
2022-07-18 17:19:27 -0400

    boilerplate ban and mute methods

--- src/ON.CreatorDashboard/Service/Models/AppSettings.cs
+++ src/ON.CreatorDashboard/Service/Models/AppSettings.cs
@@ -1,22 +1,25 @@
-﻿namespace ON.CreatorDashboard.Service.Models;
-
-public class AppSettings
+﻿namespace ON.CreatorDashboard.Service.Models
 {
-    public string DataStore { get; set; } = "/data";
+    public class AppSettings
+    {
+        public string DataStore { get; set; } = "/data";
 
-    //public bool ContainsSecrets
-    //{
-    //    get
-    //    {
-    //        if (string.IsNullOrWhiteSpace(StripeUrl))
-    //            return false;
-    //        if (string.IsNullOrWhiteSpace(StripeAccount))
-    //            return false;
-    //        if (string.IsNullOrWhiteSpace(StripeClientID))
-    //            return false;
-    //        if (string.IsNullOrWhiteSpace(StripeClientSecret))
-    //            return false;
-    //        return true;
-    //    }
-    //}
+        //public bool ContainsSecrets
+        //{
+        //    get
+        //    {
+        //        if (string.IsNullOrWhiteSpace(StripeUrl))
+        //            return false;
+        //        if (string.IsNullOrWhiteSpace(StripeAccount))
+        //            return false;
+        //        if (string.IsNullOrWhiteSpace(StripeClientID))
+        //            return false;
+        //        if (string.IsNullOrWhiteSpace(StripeClientSecret))
+        //            return false;
+        //        return true;
+        //    }
+        //}
+    }
 }
+
+
--- src/ON.CreatorDashboard/Service/Services/SubscribersService.cs
+++ src/ON.CreatorDashboard/Service/Services/SubscribersService.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ON.Fragments.CreatorDashboard.Subscribers;
 using ON.CreatorDashboard.Service.Models;
+using ON.CreatorDashboard.Service.Interfaces;
 
 namespace ON.CreatorDashboard.Service
 {
@@ -10,11 +11,36 @@ namespace ON.CreatorDashboard.Service
     {
         private readonly ILogger<SubscribersService> logger;
         private readonly IOptions<AppSettings> settings;
+        private readonly IMuteListProvider muteListProvider;
+        private readonly IBanListProvider banListProvider;
 
-        public SubscribersService(ILogger<SubscribersService> logger, IOptions<AppSettings> settings)
+        public SubscribersService(ILogger<SubscribersService> logger, IOptions<AppSettings> settings, IMuteListProvider muteListProvider, IBanListProvider banListProvider)
         {
             this.logger = logger;
             this.settings = settings;
+            this.muteListProvider = muteListProvider;
+            this.banListProvider = banListProvider;
+        }
+
+        public override Task<MuteResponse> MuteSubscriber(MuteRequest req
+            , ServerCallContext context)
+        {
+            return null;
+        }
+
+        public override Task<UnmuteResponse> UnmuteSubscriber(UnmuteRequest req, ServerCallContext context)
+        {
+            return null;
+        }
+
+        public override Task<BanResponse> BanSubscriber(BanRequest req, ServerCallContext context)
+        {
+            return null;
+        }
+
+        public override Task<UnbanResponse> UnbanSubscriber(UnbanRequest req, ServerCallContext context)
+        {
+            return null;
         }
     }
 }
