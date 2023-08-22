7be7932d79d708181223af8e242fcaa555a77f0b
Phillip Fisher <phillip_fisher@hotmail.com>
2023-08-21 19:39:14 -0500

    Allow login via email

--- src/ON.Authentication/SimpleAuth/Service/Data/IUserDataProvider.cs
+++ src/ON.Authentication/SimpleAuth/Service/Data/IUserDataProvider.cs
@@ -8,14 +8,18 @@ namespace ON.Authentication.SimpleAuth.Service.Data
 {
     public interface IUserDataProvider
     {
+        Task<bool> ChangeEmailIndex(string[] emails, Guid id);
         Task<bool> ChangeLoginIndex(string oldLoginName, string newLoginName, Guid id);
         Task<bool> Create(UserRecord user);
         Task<bool> Delete(Guid userId);
         Task<bool> Exists(Guid userId);
-        Task<bool> Exists(string loginName);
+        Task<bool> EmailExists(string email);
+        Task<bool> EmailsExist(string[] emails);
+        Task<bool> LoginExists(string loginName);
         IAsyncEnumerable<UserRecord> GetAll();
         Guid[] GetAllIds();
         Task<UserRecord> GetById(Guid userId);
+        Task<UserRecord> GetByEmail(string email);
         Task<UserRecord> GetByLogin(string loginName);
         Task Save(UserRecord user);
     }
